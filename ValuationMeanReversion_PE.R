dcf1ab2218f13ff086dfdf566423dae74802f6a5
Karo Rönty <39708710+KaroRonty@users.noreply.github.com>
2018-10-28 13:06:05 +0200

    Update ValuationMeanReversion_PE.R

--- ValuationMeanReversion_PE.R
+++ ValuationMeanReversion_PE.R
@@ -23,7 +23,17 @@ pe_data <- full_data %>%
          PE_n7 = lead(.$PE, 12 * 7),
          PE_n8 = lead(.$PE, 12 * 8),
          PE_n9 = lead(.$PE, 12 * 9),
-         PE_n10 = lead(.$PE, 12 * 10)
+         PE_n10 = lead(.$PE, 12 * 10),
+         PE_n11 = lead(.$PE, 12 * 11),
+         PE_n12 = lead(.$PE, 12 * 12),
+         PE_n13 = lead(.$PE, 12 * 13),
+         PE_n14 = lead(.$PE, 12 * 14),
+         PE_n15 = lead(.$PE, 12 * 15),
+         PE_n16 = lead(.$PE, 12 * 16),
+         PE_n17 = lead(.$PE, 12 * 17),
+         PE_n18 = lead(.$PE, 12 * 18),
+         PE_n19 = lead(.$PE, 12 * 19),
+         PE_n20 = lead(.$PE, 12 * 20)
   )
 
 # Calculate valuation deciles & their paths
@@ -49,11 +59,11 @@ pes$PE <- as.numeric(pes$PE)
 rm(list = ls(pattern = "pe_n"))
 
 # Add orders & factor levels for plotting
-pes$Year <- 0:10
+pes$Year <- 0:20
 pes$Decile <- as.factor(pes$Decile)
 pes <- transform(pes, Decile=factor(Decile,levels=mixedsort(levels(Decile), 
                                                             decreasing=TRUE)))
 # Plot
 ggplot(data = pes, aes(x = Year, y = PE)) +
   geom_line(aes(color = Decile, group = Decile), size = 1.2) +
-  scale_x_continuous(breaks = 0:10)
+  scale_x_continuous(breaks = 0:20)
